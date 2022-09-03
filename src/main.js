5d3609af01d2f505d22e1663af84c00023ed61f5
Georgiy Filippov <g.filippov777@gmail.com>
2022-09-03 13:03:09 +0500

    format

--- src/main.js
+++ src/main.js
@@ -1,11 +1,10 @@
 class TestApp {
-	init(title) {
-		console.log(`${title} Sandbox demo`)
-	}
+  init(title) {
+    console.log(`${title} Sandbox demo`)
+  }
 }
 const testApp = new TestApp()
 testApp.init('Webpack')
 
 const logger = () => console.log('test')
 logger()
-
