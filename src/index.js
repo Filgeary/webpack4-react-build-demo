5f80d632a71f4ad5d57929c70a8b26aa2347f2d0
Georgiy Filippov <g.filippov777@gmail.com>
2022-09-05 01:32:05 +0500

    add file-types loader

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import Calc from './calc'
 import Logger from './logger'
-import img from './react-logo.png'
+import imgReact from './react-logo.png'
+import imgJG from './jeffrey-goines.jpg'
 
 const calc = new Calc()
 const logger = new Logger()
 
 logger.log(calc.add(1, 2, 3, 7, 99))
 
-const image = document.createElement('img')
-image.src = img
-document.body.appendChild(image)
+const imageReact = document.createElement('img')
+imageReact.src = imgReact
+document.body.appendChild(imageReact)
+
+const imageJG = document.createElement('img')
+imageJG.src = imgJG
+const div = document.createElement('div')
+div.appendChild(imageJG)
+document.body.appendChild(div)
--- webpack.config.js
+++ webpack.config.js
@@ -3,11 +3,32 @@ module.exports = {
 
   module: {
     rules: [
+      // images
+      // ----------------------------------------
       {
-        test: /\.png$/,
+        test: /\.(png|jpg|jpeg|gif|ico)$/,
         use: [
           {
             loader: 'file-loader',
+            options: {
+              outputPath: 'images',
+              name: '[name]-[sha1:hash:7].[ext]',
+            },
+          },
+        ],
+      },
+
+      // fonts
+      // ----------------------------------------
+      {
+        test: /\.(woff|woff2|ttf|otf|eot)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              outputPath: 'fonts',
+              name: '[name].[ext]',
+            },
           },
         ],
       },
