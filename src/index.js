b2145ccf705009e2cbd25f80ce90dbee3a52b631
Georgiy Filippov <g.filippov777@gmail.com>
2022-09-05 22:07:30 +0500

    add prod config

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { App } from './App'
 import './style.scss'
-
-const App = () => <h1>Webpack</h1>
 
 ReactDOM.render(<App />, document.getElementById('root'))
--- webpack.config.js
+++ webpack.config.js
@@ -1,78 +1,90 @@
+const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
-module.exports = {
-  mode: 'development',
-  entry: './src/index.jsx',
+module.exports = (env, argv) => {
+  const devMode = argv.mode === 'development' || !argv.mode ? 'development' : ''
+  const prodMode = argv.mode === 'production' ? 'production' : ''
 
-  module: {
-    rules: [
-      // JS, JSX
-      // ----------------------------------------
-      {
-        test: /\.js|jsx$/,
-        exclude: /node_modules/,
-        use: ['babel-loader'],
-      },
+  return {
+    mode: devMode || prodMode,
+    entry: './src/index.js',
+    output: {
+      path: path.resolve(__dirname, 'build'),
+      filename: devMode ? 'main.bundle.js' : 'main-[hash:7].bundle.js',
+    },
+    resolve: {
+      extensions: ['.js', '.jsx', '.json', '.css'],
+    },
+    devtool: devMode ? 'source-map' : undefined,
+    devServer: {
+      open: true,
+    },
 
-      // CSS
-      // ----------------------------------------
-      {
-        test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader'],
-      },
+    // Loaders
+    // ==========================================
+    module: {
+      rules: [
+        // JS, JSX
+        {
+          test: /\.js|jsx$/,
+          exclude: /node_modules/,
+          use: ['babel-loader'],
+        },
 
-      // SCSS / SASS
-      // ----------------------------------------
-      {
-        test: /\.scss|sass$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
-      },
+        // CSS
+        {
+          test: /\.css$/,
+          use: [MiniCssExtractPlugin.loader, 'css-loader'],
+        },
 
-      // images
-      // ----------------------------------------
-      {
-        test: /\.(png|jpg|jpeg|gif|ico)$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              outputPath: 'images',
-              name: '[name]-[sha1:hash:7].[ext]',
+        // SCSS / SASS
+        {
+          test: /\.scss|sass$/,
+          use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+        },
+
+        // images
+        {
+          test: /\.(png|jpg|jpeg|gif|ico)$/,
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                outputPath: 'images',
+                name: '[name]-[sha1:hash:7].[ext]',
+              },
             },
-          },
-        ],
-      },
+          ],
+        },
 
-      // fonts
-      // ----------------------------------------
-      {
-        test: /\.(woff|woff2|ttf|otf|eot)$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              outputPath: 'fonts',
-              name: '[name].[ext]',
+        // fonts
+        {
+          test: /\.(woff|woff2|ttf|otf|eot)$/,
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                outputPath: 'fonts',
+                name: '[name].[ext]',
+              },
             },
-          },
-        ],
-      },
+          ],
+        },
+      ],
+    },
+
+    // Plugins
+    // ==========================================
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: 'public/index.html',
+        title: 'Webpack Sandbox demo',
+        buildTime: new Date().toLocaleString(),
+      }),
+      new MiniCssExtractPlugin({
+        filename: 'style-[hash:7].css',
+      }),
     ],
-  },
-
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: 'public/index.html',
-      title: 'Webpack Sandbox demo',
-      buildTime: new Date().toLocaleString(),
-    }),
-    new MiniCssExtractPlugin({
-      filename: 'style-[hash:7].css',
-    }),
-  ],
-
-  devServer: {
-    open: true,
-  },
+  }
 }
